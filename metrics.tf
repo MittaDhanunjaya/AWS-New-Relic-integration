d6bc102ffd315f1d8420f21dad2bf99df9fd719d
DhanunjayaMitta <102286105+DhanunjayaMitta@users.noreply.github.com>
2023-04-23 03:12:17 +0200

    more services added

--- metrics.tf
+++ metrics.tf
@@ -10,6 +10,12 @@ resource "aws_cloudwatch_metric_stream" "newrelic_metric_stream" {
   role_arn      = aws_iam_role.metric_stream_to_firehose.arn
   firehose_arn  = aws_kinesis_firehose_delivery_stream.newrelic_firehost_stream.arn
   output_format = "opentelemetry0.7"
+  dynamic "include_filter" {
+    for_each = var.cloudwatch_metric_stream_include_filter
+    content {
+      namespace = include_filter["value"]
+    }
+  }
 }
 
 resource "newrelic_cloud_aws_link_account" "newrelic_cloud_integration_pull" {
